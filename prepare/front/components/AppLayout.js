67f6835efd64756fa714a2e2cab9c9b59a6d9ae1
sangho235 <sangho235@it.gsshop.com>
2022-03-26 19:55:34 +0900

    로그인 폼 스타일 재정의 및 리렌더링 이해하기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,9 +2,16 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd'; /*Row, Col 반응형 그리드*/
+import styled from "styled-components";
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+	vertical-align: middle;
+`
+
+
 
 const AppLayout = ({children}) => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,7 +25,7 @@ const AppLayout = ({children}) => {
 					<Link href="/profile"><a>프로필</a></Link>
 				</Menu.Item>
 				<Menu.Item>
-					<Input.Search enterButton style={{verticalAlign: 'middle'}}/>
+					<SearchInput enterButton />
 				</Menu.Item>
 				<Menu.Item>
 					<Link href="/signup"><a>회원가입</a></Link>
