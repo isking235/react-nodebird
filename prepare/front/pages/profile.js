b9a31999e7b781b74c04cbe81f493e2ab6a49317
sangho235 <sangho235@it.gsshop.com>
2022-04-15 20:51:41 +0900

    faker로 실감나는 더미데이터 만들기

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Head from 'next/head';
 import {useSelector} from "react-redux";
+import Router from "next/router";
 
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
 
+
 const Profile = () => {
 	const {me} = useSelector((state) => state.user);
+	//로그인 안했을때 프로필 화면 나오지 않도록 한다.
+	useEffect(() => {
+		if(!(me && me.id)) {
+			Router.push('/');
+		}
+	},[me && me.id]);
+
+	if(!me) {
+		return null;
+	}
 	return (
 		<>
 			<Head>
