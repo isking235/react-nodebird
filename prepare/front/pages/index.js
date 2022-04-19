5de5d8fa7f27ffbdc5c0ad4fc565e993233aca1b
sangho235 <sangho235@it.gsshop.com>
2022-04-20 00:00:12 +0900

    인피니트 스크롤링 적용하기

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppLayout from '../components/AppLayout';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
+import {LOAD_POSTS_REQUEST} from "../reducers/post";
 
 const Home = () => {
+	const dispatch = useDispatch();
 	const {me} = useSelector((state) => state.user);
-	const {mainPosts} = useSelector((state) => state.post);
+	const {mainPosts, hasMorePosts, loadPostsLoading} = useSelector((state) => state.post);
+
+
+	useEffect(()=> {
+		dispatch({
+			type : LOAD_POSTS_REQUEST,
+		});
+	},[]);
+
+	useEffect(() => {
+		function onScroll() {
+			//console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight);
+			/*
+			scrollY : 얼마나 내렸는지
+			clientHeight : 화면 보이는 길이
+			scrollHeight : 총 길이
+			* */
+			if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight-300) {
+				if (hasMorePosts && !loadPostsLoading) {
+					dispatch({
+						type : LOAD_POSTS_REQUEST,
+					});
+				}
+			}
+		}
+
+		window.addEventListener('scroll', onScroll);
+		return () => {
+			window.removeEventListener('scroll', onScroll);
+		};
+	}, [hasMorePosts, loadPostsLoading]);
+
 	return (
 		<AppLayout>
 			{me && <PostForm />}
--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {all, fork, put, takeLatest, delay} from 'redux-saga/effects';
+import {all, fork, put, takeLatest, delay,throttle} from 'redux-saga/effects';
 import {
     ADD_POST_REQUEST,
     ADD_POST_SUCCESS,
@@ -9,15 +9,36 @@ import {
     ADD_COMMENT_FAILURE,
     REMOVE_POST_SUCCESS,
     REMOVE_POST_FAILURE,
-    REMOVE_POST_REQUEST,
+    REMOVE_POST_REQUEST, LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE, generateDummyPost,
 } from "../reducers/post";
 import {ADD_POST_TO_ME, REMOVE_POST_OF_ME} from "../reducers/user";
 import shortId from "shortid";
 
+/*******************************************/
+function  loadPostsAPI(data) { //*이 들어 가지 않는다.
+    return axios.post('/api/posts', data);
+}
+function* loadPosts(action) {
+    try{
+        //const result = yield call(addPostAPI, action.data)
+        yield delay(1000);
+        const id = shortId.generate();
+
+        yield put({
+            type: LOAD_POSTS_SUCCESS,
+            data: generateDummyPost(10),
+        });
+    } catch (err) {
+        yield put({ //put은 dispatch 다
+            type: LOAD_POSTS_FAILURE,
+            data: err.response.data,
+        });
+    }
+}
 
 /*******************************************/
 function  addPostAPI(data) { //*이 들어 가지 않는다.
-    return axios.post('/api/logout', data);
+    return axios.post('/api/post', data);
 }
 function* addPost(action) {
     try{
@@ -93,6 +114,10 @@ function* addComment(action) {
     }
 }
 
+function* watchLoadPosts() {
+    yield throttle(5000,LOAD_POSTS_REQUEST, loadPosts);
+}
+
 function* watchAddPost() {
     yield takeLatest(ADD_POST_REQUEST, addPost);
 }
@@ -109,6 +134,7 @@ function* watchAddComment() {
 export default function* postSaga() {
     yield all([
         fork(watchAddPost),
+        fork(watchLoadPosts),
         fork(watchRemovePost),
         fork(watchAddComment),
     ])
