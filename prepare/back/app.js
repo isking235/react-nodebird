bcd7f7f48053aaf9f7fc351af95461851dcb4ae6
sangho235 <sangho235@it.gsshop.com>
2022-05-02 08:04:42 +0900

    익스프레스로 라우터 분리하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postRouter = require('/routes/post');
 const app = express();
 /*
 REST API 정의
@@ -30,15 +31,7 @@ app.get('/api/posts', (req, res) => {
     ]);
 });
 
-app.post('/api/post', (req, res) => {
-    res.json({id: 1, content: 'hello'});
-
-});
-
-app.delete('/api/post', (req, res) => {
-    res.json({id: 1});
-});
-
+app.use('/post', postRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행 중');
