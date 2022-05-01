bf7a02d2ec7554fe48a453c19ae07b489d94b7f0
sangho235 <sangho235@it.gsshop.com>
2022-05-02 08:00:09 +0900

    익스페레스로 라우팅하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,18 +1,45 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+const express = require('express');
+const app = express();
+/*
+REST API 정의
+
+app.get -> 가져오다.
+app.post -> 생성하다.
+app.put -> 전체_수정
+app.delete -> 제거
+app.patch-> 부분_수정
+app.options -> 찔러보기
+app.head -> 헤더만 가져오기(헤더/바디)
 
 
 
+*/
+app.get('/', (req, res) => {
+    res.send('hello express');
+});
 
-    res.write('<h1>Hello node1</h1>');
-    res.write('<h2>Hello node2</h2>');
-    res.write('<h3>Hello node3</h3>');
-    res.write('<h4>Hello node4</h4>');
+app.get('/api', (req, res) => {
+    res.send('hello api');
+});
 
-    res.end('<h15Hello node5</h5>');
+app.get('/api/posts', (req, res) => {
+    res.json([
+        {id: 1, content: 'hello'},
+        {id: 2, content: 'hello2'},
+        {id: 3, content: 'hello3'},
+    ]);
+});
+
+app.post('/api/post', (req, res) => {
+    res.json({id: 1, content: 'hello'});
 
 });
-server.listen(3065, () => {
+
+app.delete('/api/post', (req, res) => {
+    res.json({id: 1});
+});
+
+
+app.listen(3065, () => {
     console.log('서버 실행 중');
 });
