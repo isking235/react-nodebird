65fa0d78f40c44aaaf05766566fb8c06b1651cbf
sangho235 <sangho235@it.gsshop.com>
2022-08-01 21:42:35 +0900

    production에서 database 명 변경

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -20,7 +20,7 @@ module.exports = {
   "production": {
     "username": "isking235",
     "password": process.env.DB_PASSWORD,
-    "database": "database_production",
+    "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
