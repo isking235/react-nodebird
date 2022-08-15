362417623e660a1b24db7e6d167a40207f186670
sangho235 <sangho235@it.gsshop.com>
2022-08-15 13:19:21 +0900

    500에로 발생한 이유 찾음, i가 정의 안됨

--- prepare/front/components/PostCardContent.js
+++ prepare/front/components/PostCardContent.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 
 const PostCardContent = ({postData}) => (// 첫 번째 게시글 #해시태그 #익스프레스 /*/g: 전체선택, /#: #, []여기있는것중선택, ^: 이건제외, \s: 공백, \s# : #앞에서 멈춰*/
     <div>
-        {postData.split(/(#[^\s#]+)/g).map((v) => {
+        {postData.split(/(#[^\s#]+)/g).map((v,i) => {
             if (v.match(/(#[^\s#]+)/)) {
                 return <Link href={`/hashtag/${v.slice(1)}`} prefetch={false} key={i}><a>{v}</a></Link>
             }
